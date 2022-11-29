aa1f316cea4efda978617db353829e5180a9182b
Pedro Blázquez <tordillos2@gmail.com>
2022-11-29 11:23:09 +0100

    Objetos como propiedades

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,5 @@
 (() => {
-    
+
     // No aplicando el principio de responsabilidad única
 
     type Gender = 'M' | 'F';
@@ -15,54 +15,91 @@
     //         this.birthdate = birthdate;
     //     }
     // }
+    interface PersonProps {
+        name: string;
+        gender: Gender;
+        birthdate: Date;
+    }
     class Person {
-        constructor(
-            public name: string, 
-            public gender: Gender, 
-            public birthdate: Date) {}
+        public name: string;
+        public gender: Gender;
+        public birthdate: Date;
+
+        constructor({ name, gender, birthdate }: PersonProps) {
+            this.name = name;
+            this.gender = gender;
+            this.birthdate = birthdate;
+        }
+    }
+    interface UserProps {
+        email: string,
+        role: string,
+        lastAcces: Date,
+        name: string;
+        gender: Gender;
+        birthdate: Date;
     }
     class User extends Person {
-        constructor(
-            public email: string,
-            public role: string,
-            private lastAcces: Date,
-            name: string,
-            gender: Gender,
-            birthdate: Date,
-        ) {
-            super(name, gender, birthdate);
+        public email: string;
+        public role: string;
+        private lastAcces: Date;
+
+        constructor({ email, role, lastAcces, name, gender, birthdate }: UserProps) {
+            super({ name, gender, birthdate });
+            this.email = email;
+            this.role = role;
+            this.lastAcces = lastAcces;
         }
         checkCredentials() {
             return true;
         }
     }
 
+    interface UserSettingProps {
+        workingDirectory: string;
+        lastOpenFolder: string;
+        email: string;
+        role: string;
+        lastAcces: Date;
+        name: string;
+        gender: Gender;
+        birthdate: Date;
+    }
     class UserSetting extends User {
+
+        public workingDirectory: string;
+        public lastOpenFolder: string;
+
         constructor(
-            public workingDirectory: string,
-            public lastOpenFolder: string,
-            email: string,
-            role: string,
-            lastAcces: Date,
-            name: string,
-            gender: Gender,
-            birthdate: Date,
+            { 
+                workingDirectory, 
+                lastOpenFolder, 
+                email, 
+                role, 
+                lastAcces, 
+                name,
+                gender,
+                birthdate 
+            }: UserSettingProps
         ) {
-            super(email, role, lastAcces, name, gender, birthdate);
+            super({ email, role, lastAcces, name, gender, birthdate });
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder = lastOpenFolder;
         }
     }
 
-    const userSetting = new UserSetting(
-        '/usr/home',
-        '/home',
-        'email@email',
-        'Admin',
-        new Date('1998-10-21'),
-        'prueba',
-        'M',
-        new Date('1998-10-21')
+    const userSetting = new UserSetting( {
+        workingDirectory: '/usr/home',
+        lastOpenFolder: '/home',
+        email: 'email@email',
+        role: 'Admin',
+        lastAcces: new Date('1998-10-21'),
+        name: 'prueba',
+        gender: 'M',
+        birthdate: new Date('1998-10-21')
+    }
     );
 
-    console.log(userSetting);
+    // console.log(userSetting);
 
 })();
