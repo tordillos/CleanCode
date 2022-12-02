2afacbaaf6e481770c3b72d1050b0daf8c6848fc
Pedro Blázquez <tordillos2@gmail.com>
2022-12-02 11:49:47 +0100

    Ejemplo SRP - Responsabilidad Única 02 implementandolo

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -4,37 +4,68 @@
         id:   number;
         name: string;
     }
-    
+    class ProductService {
+        getProduct( id: number) {
+            console.log('Producto: ',{ id, name: 'OLED Tv' });
+        }
+
+        saveProduct( product: Product) {
+            console.log('Guardando en base de datos', product );
+        }
+    }
+    class Mailer {
+
+        private masterEmail: string = 'tordillos2@gmail.com';
+
+        sendEmail(emailList: string[], template: 'to-clients' | 'to-admins') {
+            console.log('Enviando correo a los clientes', emailList)
+        }
+
+    }
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
     class ProductBloc {
-    
+        private productService: ProductService;
+
+        private mailer: Mailer;
+
+        constructor(productService: ProductService, mailer: Mailer) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
+
         loadProduct( id: number ) {
-            // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            this.productService.getProduct(id);
         }
     
         saveProduct( product: Product ) {
-            // Realiza una petición para salvar en base de datos 
-            console.log('Guardando en base de datos', product );
+            this.productService.saveProduct(product);
         }
-    
+
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            this.mailer.sendEmail(['prueba@email.com'], 'to-clients');
         }
     
+    }
+    class CartBloc {
+        
+        private itemsInCart: Object[] = [];
+
         onAddToCart( productId: number ) {
             // Agregar al carrito de compras
             console.log('Agregando al carrito ', productId );
         }
-    
     }
-    
-    const productBloc = new ProductBloc();
+
+    const productService = new ProductService();
+    const mailer = new Mailer();
+
+    const productBloc = new ProductBloc(productService, mailer);
+    const cartBloc = new CartBloc();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
-    productBloc.onAddToCart(10);
+    cartBloc.onAddToCart(10);
 
 })();
