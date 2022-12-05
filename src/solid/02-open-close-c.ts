d88076321cd93f7edfd67202cfe945e5ac150c08
Pedro Blázquez <tordillos2@gmail.com>
2022-12-05 17:12:45 +0100

    Remover dependencia axios

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,11 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
 export class HttppClient {
-    
+
+    // async get(url: string) {
+    //     const { data, status } = await axios.get(url);
+    //     console.log({ status });
+    //     return { data, status };
+    // }
+
     async get(url: string) {
-        const { data, status } = await axios.get(url);
-        console.log({ status });
-        return { data, status };
+        const  resp = await fetch(url);
+        const data = await resp.json();
+
+        return { data, status: resp.status };
     }
 
 }
