edf1b915e8e8e6a0fb38d328e2e89869aa9cabff
Pedro Blázquez <tordillos2@gmail.com>
2023-02-01 17:05:41 +0100

    Solución segregación de interfaz

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,30 +1,33 @@
 // Estan implementando los mismos métodos
 interface Bird {
-    fly():void;
     eat():void;
+}
+interface FlyingBird {
+    fly():void;
+}
+interface RunningBird {
     run():void;
 }
-class Tucan implements Bird {
+interface SwimmerBird {
+    swim(): void;
+}
+class Tucan implements Bird, FlyingBird {
     public fly(): void { }
     public eat(): void { }
-    public run(): void { }
 }
 
-class Humminbird implements Bird {
+class Humminbird implements Bird, FlyingBird {
     public fly(): void { }
     public eat(): void { }
-    public run(): void { }
 }
 // El avestruz no vuela... depende de la implementación de Bird
-class Ostrich implements Bird {
-    public fly(): void { }
+class Ostrich implements Bird, RunningBird {
     public eat(): void { }
     public run(): void { }
 }
 // El pinguino no vuela pero nada y tendría que implementarlo en Bird...
 // Tenemos que hacer la segregación por diferentes interfaces
-class Penguin implements Bird {
-    public fly(): void { }
+class Penguin implements Bird, RunningBird, SwimmerBird {
     public eat(): void { }
     public run(): void { }
     public swim(): void {}
