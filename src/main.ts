a859f96772eb16981a446948c570cf4655e59c87
Pedro Blázquez <tordillos2@gmail.com>
2023-02-01 17:48:45 +0100

    Problema de inversión de dependencias

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 // import './solid/02-open-close-a';
-import './solid/03-liskov-a';
+// import './solid/03-liskov-a';
+import './solid/05-dependency-a';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
